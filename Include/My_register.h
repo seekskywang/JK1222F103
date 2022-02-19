3446511b91ba513e0dbce5bf9c5b04b630b5968b
seekskywang <seekskywang@gmail.com>
2022-02-19 10:18:21 +0800

    1

--- Include/My_register.h
+++ Include/My_register.h
@@ -286,12 +286,14 @@ extern vu16 SWDelay;
 #define SWDELAY   1000 
 //============================================================================= 
 #define Receive_BUFFERSIZE   10
-#define V_LOW_MAX   120000   //电压低档最高电压
-#define V_HIG_MAX   120000   //电压高档最高电压
+//#define V_LOW_MAX   120000   //电压低档最高电压
+//#define V_HIG_MAX   120000   //电压高档最高电压
+#define V_LOW_MAX   500000   //电压低档最高电压
+#define V_HIG_MAX   500000   //电压高档最高电压
 #define I_LOW_MAX   60000   //电流低档位最高限制电流
-//#define POWER_MAX   1200000   //电流低档位最高限制功率
+#define POWER_MAX   1200000   //电流低档位最高限制功率
 //#define POWER_MAX   800000   //电流低档位最高限制功率
-#define POWER_MAX   600000   //电流低档位最高限制功率
+//#define POWER_MAX   600000   //电流低档位最高限制功率
 //=============================================================================
 #endif
 /******************* (C) COPYRIGHT 2015 KUNKIN *****END OF FILE*************************/
